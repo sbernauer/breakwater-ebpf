50c3de1af76e0b60fbec0941f699fb029c1be469
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-08-10 20:23:13 +0200

    Switch from env_logger to simplelog to make logging working

--- breakwater-ebpf/src/main.rs
+++ breakwater-ebpf/src/main.rs
@@ -3,7 +3,8 @@ use anyhow::Context;
 use aya::programs::{Xdp, XdpFlags};
 use aya_log::BpfLogger;
 use clap::Parser;
-use log::info;
+use log::{info, LevelFilter};
+use simplelog::{TermLogger, ConfigBuilder, ColorChoice, TerminalMode};
 use tokio::signal;
 
 #[derive(Debug, Parser)]
@@ -16,7 +17,15 @@ struct Opt {
 async fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::parse();
 
-    env_logger::init();
+    TermLogger::init(
+        LevelFilter::Info,
+        ConfigBuilder::new()
+            .set_target_level(LevelFilter::Error)
+            .set_location_level(LevelFilter::Error)
+            .build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )?;
 
     // This will include your eBPF object file as raw bytes at compile-time and load it at
     // runtime. This approach is recommended for most real-world use cases. If you would
